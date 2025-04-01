69a350edbc07fbef654a78b53f6464fb51f51f2e
berrgrayson <niyongaboberrygray@gmail.com>
2025-04-01 20:02:26 +0200

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,21 @@ function Home() {
     { id: 2, title: 'Terminator', release_date: '1999' },
     { id: 3, title: 'The Matrix', release_date: '1998' },
   ];
+
+  const handleSearch = () => {};
+
   return (
     <div className='home'>
+      <form onSubmit={handleSearch} className='search-form'>
+        <input
+          type='text'
+          placeholder='Search for movies...'
+          className='search-input'
+        />
+        <button type='submit' className='search-button'>
+          Search
+        </button>
+      </form>
       <div className='movies-grid'>
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
@@ -16,3 +29,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
