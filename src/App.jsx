9cb91b468bac2655a0f0cc866827bd3b7b11edca
berrgrayson <niyongaboberrygray@gmail.com>
2025-04-02 16:59:09 +0200

    Add Movies API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css';
+import './css/App.css';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import { Routes, Route } from 'react-router-dom';
+import { MovieProvider } from './contexts/MovieContext';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className='main-content'>
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path='/favorites' element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
